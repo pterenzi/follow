29ac0f6ef46baf6f66613f378290743eaa6cd852
Marcio Aun Migueis <marcioa1@gmail.com>
2009-04-15 16:22:16 -0300

    faltou migration

---
+++ db/migrate/20090415185311_remove_executor_from_tarefas.rb
@@ -0,0 +1,9 @@
+class RemoveExecutorFromTarefas < ActiveRecord::Migration
+  def self.up
+    remove_column :tarefas, :usuario_executor_id
+  end
+
+  def self.down
+    add_column :tarefas, :usuario_executor_id, :integer
+  end
+end
