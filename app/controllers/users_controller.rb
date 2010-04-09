58e333a8775b52fed9ab7fe186b4c335e31ebf68
Paulo Terenzi <pterenzi@gmail.com>
2010-04-09 01:29:36 -0300

    jNice forms

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -63,6 +63,16 @@ class UsersController < ApplicationController
     end
   end
   
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+
+    respond_to do |format|
+      format.html { redirect_to(users_url) }
+      format.xml  { head :ok }
+    end
+  end
+  
   def change_password
      @user = User.find(params[:id])
   end
